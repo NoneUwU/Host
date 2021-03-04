42c0de6321b15bf0b028ef580313cc3914a924e6
NoneUwU <79335750+NoneUwU@users.noreply.github.com>
2021-03-04 18:22:28 -0400

    Update RoleDelete.js

--- commands/info/RoleDelete.js
+++ commands/info/RoleDelete.js
@@ -5,6 +5,7 @@ module.exports  = {
     name: "deleterole",
     aliases : ['dr'],
     run : async(client, message, args) => {
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("No tienes permiso para usar este comando")
         const member = message.mentions.members.first()
         if(!member) return message.reply('Mensiona el usuario a dar el role')
 
