800ec2a955d91f7e013ec8c2f1368710b72788ae
NoneUwU <79335750+NoneUwU@users.noreply.github.com>
2021-04-02 20:06:12 -0400

    Delete events directory

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const client = require('../index')
-const config = require('../config.json')
-const prefix = config.prefix
-const ms = require('ms');
-const Schema = require('../models/member-count');
-
-client.on('ready',() => {
-    console.log(`${client.user.username} Ha iniciado Correctamante.`);
-});
