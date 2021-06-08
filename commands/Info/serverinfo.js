43eae598ab7e323c36e83f8f266c0b41e5c7b435
NoneUwU <79335750+NoneUwU@users.noreply.github.com>
2021-06-08 14:43:20 -0400

    Update serverinfo.js

--- commands/Info/serverinfo.js
+++ commands/Info/serverinfo.js
@@ -6,32 +6,32 @@ const utils = require('utils')
 
 const filterLevels = {
 	DISABLED: 'Off',
-	MEMBERS_WITHOUT_ROLES: 'No Role',
+	MEMBERS_WITHOUT_ROLES: 'Sin rol',
 	ALL_MEMBERS: 'Everyone'
 };
 
 const verificationLevels = {
-	NONE: 'None',
-	LOW: 'Low',
-	MEDIUM: 'Medium',
+	NONE: 'Ninguna',
+	LOW: 'Baja',
+	MEDIUM: 'Media',
 	HIGH: '(╯°□°）╯︵ ┻━┻',
 	VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
 };
 
 const regions = {
 	brazil: 'Brazil',
-	europe: 'Europe',
+	europe: 'Europa',
 	hongkong: 'Hong Kong',
 	india: 'India',
-	japan: 'Japan',
-	russia: 'Russia',
-	singapore: 'Singapore',
-	southafrica: 'South Africa',
+	japan: 'Japon',
+	russia: 'Rusia',
+	singapore: 'Singapur',
+	southafrica: 'Sur Africa',
 	sydeny: 'Sydeny',
 	'us-central': 'US Central',
-	'us-east': 'US East',
-	'us-west': 'US West',
-	'us-south': 'US South'
+	'us-east': 'US Este',
+	'us-west': 'US Norte',
+	'us-south': 'US Sur'
 };
 
 module.exports  = {
@@ -46,41 +46,40 @@ module.exports  = {
 		const emojis = message.guild.emojis.cache;
 
 		const embed = new MessageEmbed()
-			.setDescription(`**Guild information for __${message.guild.name}__**`)
+			.setDescription(`**Informacion de  __${message.guild.name}__**`)
 			.setColor('BLUE')
 			.setThumbnail(message.guild.iconURL({ dynamic: true }))
 			.addField('General', [
-				`**❯ Name:** ${message.guild.name}`,
+				`**❯ Nombre:** ${message.guild.name}`,
 				`**❯ ID:** ${message.guild.id}`,
 				`**❯ Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
 				`**❯ Region:** ${regions[message.guild.region]}`,
-				`**❯ Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
-				`**❯ Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
-				`**❯ Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
-				`**❯ Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+				`**❯ Nivel de Bosst:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+				`**❯ Filtro de Expicidad:** ${filterLevels[message.guild.explicitContentFilter]}`,
+				`**❯ Nivel de Verificacion:** ${verificationLevels[message.guild.verificationLevel]}`,
+				`**❯ creado:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
 				'\u200b'
 			])
-			.addField('Statistics', [
-				`**❯ Role Count:** ${roles.length}`,
-				`**❯ Emoji Count:** ${emojis.size}`,
-				`**❯ Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
-				`**❯ Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
-				`**❯ Member Count:** ${message.guild.memberCount}`,
-				`**❯ Humans:** ${members.filter(member => !member.user.bot).size}`,
+			.addField('Stadisticas', [
+				`**❯ Roles:** ${roles.length}`,
+				`**❯ Emojis:** ${emojis.size}`,
+				`**❯ Emojis Normales:** ${emojis.filter(emoji => !emoji.animated).size}`,
+				`**❯ Emojis Animados:** ${emojis.filter(emoji => emoji.animated).size}`,
+				`**❯ Miembros:** ${message.guild.memberCount}`,
+				`**❯ Humanos:** ${members.filter(member => !member.user.bot).size}`,
 				`**❯ Bots:** ${members.filter(member => member.user.bot).size}`,
-				`**❯ Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
-				`**❯ Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
-				`**❯ Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
+				`**❯ Canales de Texto:** ${channels.filter(channel => channel.type === 'text').size}`,
+				`**❯ Canales de Voz:** ${channels.filter(channel => channel.type === 'voice').size}`,
+				`**❯ Bosters:** ${message.guild.premiumSubscriptionCount || '0'}`,
 				'\u200b'
 			])
-			.addField('Presence', [
+			.addField('Precencias', [
 				`**❯ Online:** ${members.filter(member => member.presence.status === 'online').size}`,
-				`**❯ Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
-				`**❯ Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
+				`**❯ Ausente:** ${members.filter(member => member.presence.status === 'idle').size}`,
+				`**❯ No Molestar:** ${members.filter(member => member.presence.status === 'dnd').size}`,
 				`**❯ Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
 				'\u200b'
 			])
-			.addField(`Roles [${roles.length - 1}]`, roles.join(', '))
 			.setTimestamp();
 		message.channel.send(embed);
     }
